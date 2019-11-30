3809ee0c5b1f8ebc96bca358de4cf0429177786d
Yiyao Zhou <zhou0160@algonquinlive.com>
2019-11-30 13:21:45 -0500

    basic files

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {FlatList } from 'react-native'
+import { Container, Text, ListItem, Button, Body, Right, Icon} from 'native-base';
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
+
+export default class MovieList extends Component {
+
+  
+  render() {
+
+    // if(!this.state.isReady) return <AppLoading/>
+
+    return (
+        <Container>
+        </Container>
+    )
+}
+}
